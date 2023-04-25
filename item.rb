bc3c13f57055222b65c85fa10cf573eef6d84299
Dennis Every <dennis@ansyra.com>
2023-04-25 09:44:37 -0400

    Update Item class #initialize with genre, author, source, label

--- item.rb
+++ item.rb
@@ -1,2 +1,10 @@
 class Item
+  attr_reader :genre, :author, :source, :label
+
+  def initialize(genre, author, source, label)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+  end
 end
