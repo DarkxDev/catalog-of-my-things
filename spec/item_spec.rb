36807450b03167b3242a9f47ef3d331ba28b772f
Dennis Every <dennis@ansyra.com>
2023-04-25 09:42:06 -0400

    Add spec for on create Item, properties visible in the diagram should be defined and set up in the constructor method

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,4 +1,18 @@
 require './item'
 
 describe Item do
+  describe '#initialize' do
+    let(:genre) { double('genre') }
+    let(:author) { double('author') }
+    let(:source) { double('source') }
+    let(:label) { double('label') }
+
+    it 'creates a new item with genre, author, source, and label' do
+      item = Item.new(genre, author, source, label)
+      expect(item.genre).to eq genre
+      expect(item.author).to eq author
+      expect(item.source).to eq source
+      expect(item.label).to eq label
+    end
+  end
 end
