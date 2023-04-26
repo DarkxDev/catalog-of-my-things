5c44cda70b87f9c84448ce4d7ed0866cffbbe882
Dennis Every <dennis@ansyra.com>
2023-04-26 08:47:37 -0400

    Add spec Author#add_item

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -10,4 +10,14 @@ describe Author do
       expect(author.id).to eq(5)
     end
   end
+
+  describe '#add_item' do
+    it 'expect to add_item to the items collection' do
+      item = Item.new('genre', 'author', 'source', 'label', 'publish_date')
+      author = Author.new('John', 'Doe')
+      author.add_item(item)
+
+      expect(author.items.last).to eq(item)
+    end
+  end
 end
