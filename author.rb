11029b4bec6f62820ed0655b1afe678efc8d9e60
Dennis Every <dennis@ansyra.com>
2023-04-26 08:44:41 -0400

    Add Author#initialze

---
+++ author.rb
@@ -0,0 +1,10 @@
+class Author
+  attr_accessor :id, :first_name, :last_name, :items
+
+  def initialize(first_name, last_name)
+    @id = Time.now.to_i
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+end
