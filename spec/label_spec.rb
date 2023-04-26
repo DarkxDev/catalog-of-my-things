fcad9a1762ab4b6d1bb006b6e7d9be6ed54173f6
Dennis Every <dennis@ansyra.com>
2023-04-26 07:59:30 -0400

    Add spec Label#add_item

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,11 +1,21 @@
 require './label'
 
 describe Label do
+  let(:item) { double('item') }
+
   describe '#initialize' do
     it 'creates a new label with id, title, color, items' do
       label = Label.new('title', 'color')
       expect(label.title).to eq('title')
       expect(label.color).to eq('color')
+    end
+  end
+
+  describe '#add_item' do
+    it 'expect to add_item to the items collection' do
+      label = Label.new('title', 'color')
+      label.add_item(item)
+      expect(label.items.last).to eq(item)
     end
   end
 end
